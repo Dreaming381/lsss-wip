7024b18b7006f1a2d25f911a8a9602b356fc1586
Tyler <dreamingimlatios@gmail.com>
2020-06-20 20:00:21 -0500

    Upgrade packages, switch to HR_V2, and fix AI infinite wall bug
    Packages are all updated to the latest. Unfortunately, the new DOTS Editor package doesn't resolve any issues.
    
    Switching to HDRP 9.0.0 required interacting with the shader graphs and materials before things started rendering again. Hybrid Renderer V1 does not work correctly with _Fade. V2 works with _Fade, but does not render wormholes correctly.
    
    V2 is very fast CPU-wise, but seems to be hitting the GPU harder. Or maybe I just never noticed the GPU overhead before. Regardless, I am now GPU-bound. Crazy! This was not meant to be a graphically-intense project. I'm curious if it is faster in a build, but won't worry about that right now. I need working wormholes first. I did disable a few HDRP features. I also had to turn down the motion blur because that's a thing V2 does. Now there's just enough to make it look really good!
    
    The AI bug was caused by AI spawning near the arena boundary aimed toward the boundary. Their default destination then ended up outside the boundary.
    
    Also I shrunk the arena of the massive battle scene. The battle is definitely more epic. Over 45,000 bullets were flying at the simulation peak!

--- Assets/_Code/SubSystems/AI/AiInitializeNewShipsSystem.cs
+++ Assets/_Code/SubSystems/AI/AiInitializeNewShipsSystem.cs
@@ -15,10 +15,13 @@ namespace Lsss
 
         protected override void OnUpdate()
         {
+            float arenaRadius = sceneGlobalEntity.GetComponentData<ArenaRadius>().radius;
             Entities.WithAll<AiInitializeTag>().WithStoreEntityQueryInField(ref m_query).ForEach((ref AiDestination targetPosition, in Translation trans, in Rotation rot,
                                                                                                   in AiPersonality personality) =>
             {
                 targetPosition.position = math.forward(rot.Value) * (personality.spawnForwardDistance + personality.destinationRadius) + trans.Value;
+                var radius              = math.length(targetPosition.position);
+                targetPosition.position = math.select(targetPosition.position, targetPosition.position * arenaRadius / radius, radius > arenaRadius);
             }).Run();
 
             EntityManager.RemoveComponent<AiInitializeTag>(m_query);
