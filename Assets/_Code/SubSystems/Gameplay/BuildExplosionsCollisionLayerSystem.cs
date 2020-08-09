37c8245acc96109bdb6084a5d020bca913def3d8
Tyler <dreamingimlatios@gmail.com>
2020-08-09 15:34:56 -0500

    Fix explosion collision/graphic mismatch
    Too often players were getting caught by the explosion. This is much more forgiving. I may need to increase the radius of the explosion now.

--- Assets/_Code/SubSystems/Gameplay/BuildExplosionsCollisionLayerSystem.cs
+++ Assets/_Code/SubSystems/Gameplay/BuildExplosionsCollisionLayerSystem.cs
@@ -19,7 +19,7 @@ namespace Lsss
             var bodies = new NativeArray<ColliderBody>(m_query.CalculateEntityCount(), Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
             Entities.WithAll<ExplosionTag>().WithStoreEntityQueryInField(ref m_query).ForEach((Entity entity, int entityInQueryIndex, in Translation translation, in Scale scale) =>
             {
-                var sphere                 = new SphereCollider(0f, scale.Value);
+                var sphere                 = new SphereCollider(0f, scale.Value / 2f);  //convert diameter to radius
                 bodies[entityInQueryIndex] = new ColliderBody
                 {
                     entity    = entity,
