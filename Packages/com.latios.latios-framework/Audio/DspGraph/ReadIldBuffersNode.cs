50f26786741614f0f2fb9453e5708ef135e11309
Tyler <dreamingimlatios@gmail.com>
2020-12-14 01:13:54 -0600

    Audio Weekend 1 Checkpoint
    And so this next adventure begins. This is the first piece of the puzzle foreshadowing how this audio system is getting put together.
    
    There's a couple of pieces that are different from the original prototype though. For starters, I am doing the ILD processing on the audio thread rather than the job threads. I might choose to do this differently later depending on performance. But for now I am doing it this way as I won't need to keep a running SVF history.
    
    Second, I am implementing subframes which account for when the audio framerate is faster than the graphical framerate.
    
    Third, I have a boolean to enable or disable warnings.
    
    And fourth, I plan to expose the DSPGraph more directly rather than hide it. These means users will be able to blend the sound effects from ECS with custom audio for unique objects and background tracks.
    
    My next step is to implement the LPF node and the ILD mixdown node. But that may not be for a while as I will be out of town and away from my prototype implementation to use as reference. So instead I will probably do some LSSS UI work.

---
+++ Packages/com.latios.latios-framework/Audio/DspGraph/ReadIldBuffersNode.cs
@@ -0,0 +1,112 @@
+﻿using Unity.Audio;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace Latios.Audio
+{
+    [BurstCompile(CompileSynchronously = true)]
+    public unsafe struct ReadIldBuffersNode : IAudioKernel<ReadIldBuffersNode.Parameters, ReadIldBuffersNode.SampleProviders>
+    {
+        public enum Parameters
+        {
+            Unused
+        }
+        public enum SampleProviders
+        {
+            Unused
+        }
+
+        int       m_currentFrame;
+        int       m_currentSubframe;
+        int       m_lastPlayedBufferID;
+        IldBuffer m_ildBuffer;
+
+        internal IldBuffer m_queuedIldBuffer;
+
+        public void Initialize()
+        {
+            m_currentFrame       = 0;
+            m_currentSubframe    = 0;
+            m_lastPlayedBufferID = -1;
+            m_ildBuffer          = default;
+            m_queuedIldBuffer    = default;
+        }
+
+        public void Execute(ref ExecuteContext<Parameters, SampleProviders> context)
+        {
+            bool bufferStarved = false;
+
+            m_currentSubframe++;
+            if (m_currentSubframe >= m_ildBuffer.subframesPerFrame)
+            {
+                m_currentSubframe = 0;
+                m_currentFrame++;
+                m_lastPlayedBufferID = m_ildBuffer.bufferId;
+                m_ildBuffer          = m_queuedIldBuffer;
+            }
+
+            for (int outputChannelIndex = 0; outputChannelIndex < context.Outputs.Count; outputChannelIndex++)
+            {
+                var channelOutput = context.Outputs.GetSampleBuffer(outputChannelIndex);
+                var outputBuffer  = channelOutput.GetBuffer(0);
+                if (m_ildBuffer.leftBufferChannels.Length + m_ildBuffer.rightBufferChannels.Length < context.Outputs.Count)
+                {
+                    for (int i = 0; i < outputBuffer.Length; i++)
+                    {
+                        outputBuffer[i] = 0f;
+                    }
+                }
+                else if (m_currentFrame - m_ildBuffer.frame >= m_ildBuffer.framesInBuffer)
+                {
+                    for (int i = 0; i < outputBuffer.Length; i++)
+                    {
+                        outputBuffer[i] = 0f;
+                    }
+                    bufferStarved = true;
+                }
+                else
+                {
+                    bool useLeft               = outputChannelIndex < m_ildBuffer.leftBufferChannels.Length;
+                    int  ildBufferChannelIndex = math.select(outputChannelIndex - m_ildBuffer.leftBufferChannels.Length, outputChannelIndex, useLeft);
+                    var  ildBufferChannel      = useLeft ? m_ildBuffer.leftBufferChannels[ildBufferChannelIndex] : m_ildBuffer.rightBufferChannels[ildBufferChannelIndex];
+
+                    int bufferOffset  = m_ildBuffer.subframesPerFrame * (m_currentFrame - m_ildBuffer.frame) + m_currentSubframe;
+                    bufferOffset     *= outputBuffer.Length;
+                    for (int i = 0; i < outputBuffer.Length; i++)
+                    {
+                        outputBuffer[i] = ildBufferChannel.buffer[bufferOffset + i];
+                    }
+                }
+            }
+
+            if (bufferStarved && m_ildBuffer.warnIfStarved)
+            {
+                UnityEngine.Debug.LogWarning(
+                    $"Dsp buffer starved. Kernel frame: {m_currentFrame}, IldBuffer frame: {m_ildBuffer.frame}, ildBuffer Id: {m_ildBuffer.bufferId}, frames in buffer: {m_ildBuffer.framesInBuffer}, subframe: {m_currentSubframe}/{m_ildBuffer.subframesPerFrame}");
+            }
+
+            if (m_currentSubframe == 0)
+            {
+                //Todo: Dispatch both to the ECS job chain for realtime audio frame updates and to the main thread for buffer disposal.
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    internal unsafe struct MixWithDfgNodeUpdate : IAudioKernelUpdate<ReadIldBuffersNode.Parameters, ReadIldBuffersNode.SampleProviders, ReadIldBuffersNode>
+    {
+        public IldBuffer ildBuffer;
+
+        public void Update(ref ReadIldBuffersNode audioKernel)
+        {
+            audioKernel.m_queuedIldBuffer = ildBuffer;
+        }
+    }
+}
+
---
+++ Packages/com.latios.latios-framework/Audio/Interop/InteropStructures.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Latios.Audio
+{
+    internal unsafe struct IldBufferChannel
+    {
+        public float* buffer;
+    }
+
+    internal struct IldBuffer
+    {
+        public FixedList64<IldBufferChannel> leftBufferChannels;
+        public FixedList64<IldBufferChannel> rightBufferChannels;
+        public int                           frame;
+        public int                           bufferId;
+        public int                           framesInBuffer;
+        public int                           subframesPerFrame;
+        public bool                          warnIfStarved;
+    }
+}
+
