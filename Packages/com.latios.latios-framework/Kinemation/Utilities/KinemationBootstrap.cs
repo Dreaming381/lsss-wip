e6cb8476f4ae62f98b89c500ec3d0243b139700d
Tyler <dreamingimlatios@gmail.com>
2023-07-15 21:45:04 -0500

    Add TriMesh Colliders
    TriMesh Colliders are here! Are they optimal? Nope. Are they well-tested? Not really.
    
    But hey, they are here. And they will be improved over time.
    
    Going to sync across projects, and then time to move on to the next feature.

--- Packages/com.latios.latios-framework/Kinemation/Utilities/KinemationBootstrap.cs
+++ Packages/com.latios.latios-framework/Kinemation/Utilities/KinemationBootstrap.cs
@@ -9,6 +9,7 @@ namespace Latios.Kinemation
         /// <summary>
         /// Installs the Kinemation renderer and additional Kinemation systems, and disables some Entities.Graphics systems which Kinemation replaces.
         /// This must be installed in a LatiosWorldUnmanaged, but can be safely installed in ICustomEditorBootstrap.
+        /// This should be installed in both the Editor and runtime worlds.
         /// </summary>
         /// <param name="world">The World to install Kinemation into. Must be a LatiosWorldUnmanaged.</param>
         public static void InstallKinemation(World world)
@@ -54,11 +55,14 @@ namespace Latios.Kinemation
             BootstrapTools.InjectSystem(TypeManager.GetSystemTypeIndex<UpdateMatrixPreviousSystem>(),                       world);
         }
 
+        /// <summary>
+        /// Installs the Kinemation Mecanim state machine runtime systems. This should only be installed in the runtime world.
+        /// </summary>
+        /// <param name="world"></param>
         public static void InstallMecanimFeatures(World world)
         {
-            BootstrapTools.InjectSystem(TypeManager.GetSystemTypeIndex<KinemationAnimatorStateMachineSuperSystem>(),        world);
+            BootstrapTools.InjectSystem(TypeManager.GetSystemTypeIndex<KinemationAnimatorStateMachineSuperSystem>(), world);
         }
-
     }
 }
 
