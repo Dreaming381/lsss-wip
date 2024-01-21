167a47b97df525ccaa8b54224cfaf9dbbe8e9469
Tyler <dreamingimlatios@gmail.com>
2024-01-21 01:01:08 -0600

    Add thread indices test
    Turns out that JobsUtility.ThreadIndex takes about the same amount of time as an Rng random int. The job that queries from JobsUtility takes twice as long as the job that uses injected index. For the unfamiliar, that might sound scary, but this is actually quite good. It means that the mechanism for querying the index likely doesn't require hitting the kernel to compare native thread IDs. Unity has a fast implementation and the overhead primarily comes from the function call itself. This is totally usable for batch allocations.
    
    Next up will probably be the thread allocators and job allocators, and then I will either investigate determinism or FindPairs optimizations, depending on the survey results progress.

---
+++ Assets/_Code/OptimizationTests/ThreadIndicesTest.cs
@@ -0,0 +1,96 @@
+using Latios;
+using NUnit.Framework;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.PerformanceTesting;
+
+namespace OptimizationTests
+{
+    public class ThreadIndicesTest
+    {
+        [BurstCompile]
+        struct InjectedIndexJob : IJob
+        {
+            public NativeArray<int> writeZone;
+            public Rng              writeScatterer;
+
+            public int iterations;
+
+            [NativeSetThreadIndex]
+            int m_threadIndex;
+
+            public void Execute()
+            {
+                var rng = writeScatterer.GetSequence(m_threadIndex);
+                for (int i = 0; i < iterations; i++)
+                {
+                    writeZone[rng.NextInt(0, writeZone.Length)] += m_threadIndex;
+                }
+            }
+        }
+
+        [BurstCompile]
+        struct QueriedIndexJob : IJob
+        {
+            public NativeArray<int> writeZone;
+            public Rng              writeScatterer;
+
+            public int iterations;
+
+            public void Execute()
+            {
+                var rng = writeScatterer.GetSequence(Unity.Jobs.LowLevel.Unsafe.JobsUtility.ThreadIndex);
+                for (int i = 0; i < iterations; i++)
+                {
+                    writeZone[rng.NextInt(0, writeZone.Length)] += Unity.Jobs.LowLevel.Unsafe.JobsUtility.ThreadIndex;
+                }
+            }
+        }
+
+        [Test, Performance]
+        public void CompareIndicesAccess()
+        {
+            var arrayInjected = new NativeArray<int>(100, Allocator.TempJob);
+            var arrayQueried  = new NativeArray<int>(100, Allocator.TempJob);
+
+            var iterations = 10000000;
+            var rng        = new Rng("ThreadIndicesTest");
+
+            var injectedJob = new InjectedIndexJob
+            {
+                writeZone      = arrayInjected,
+                iterations     = 10,
+                writeScatterer = rng
+            };
+
+            var queriedJob = new QueriedIndexJob
+            {
+                writeZone      = arrayQueried,
+                iterations     = 10,
+                writeScatterer = rng
+            };
+
+            injectedJob.Schedule().Complete();
+            queriedJob.Schedule().Complete();
+            injectedJob.iterations = iterations;
+            queriedJob.iterations  = iterations;
+
+            Measure.Method(() => { injectedJob.Schedule().Complete(); })
+            .SampleGroup(new SampleGroup("Injected", SampleUnit.Millisecond))
+            .WarmupCount(0)
+            .MeasurementCount(1)
+            .Run();
+
+            Measure.Method(() => { queriedJob.Schedule().Complete(); })
+            .SampleGroup(new SampleGroup("Queried", SampleUnit.Millisecond))
+            .WarmupCount(0)
+            .MeasurementCount(1)
+            .Run();
+        }
+    }
+}
+
