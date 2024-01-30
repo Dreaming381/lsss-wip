8928ee4997be5d92a391acab3b4765ab9f5cc3f0
Tyler <dreamingimlatios@gmail.com>
2024-01-30 01:42:00 -0600

    Add runtime skeleton blob builders
    This is a new and probably really fragile API for building SkeletonClipSetBlobs at runtime or in editor tools. It handles the shadow hierarchy backend stuff and all the sampling and exposes the important details in two disposable struct types.

--- Packages/com.latios.latios-framework/Kinemation/Authoring/ExcludeFromSkeletonAuthoring.cs
+++ Packages/com.latios.latios-framework/Kinemation/Authoring/ExcludeFromSkeletonAuthoring.cs
@@ -9,6 +9,8 @@ namespace Latios.Kinemation.Authoring
     /// Excludes the GameObject from becoming a bone in the skeleton, whether exposed or optimized (exported).
     /// This does not prevent skinned meshes from being bound to the skeleton if the mesh has bindposes.
     /// </summary>
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Latios/Kinemation/Exclude From Skeleton")]
     public class ExcludeFromSkeletonAuthoring : MonoBehaviour
     {
     }
